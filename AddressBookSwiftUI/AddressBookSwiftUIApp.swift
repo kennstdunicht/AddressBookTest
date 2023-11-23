a6757a33c15ea48cd2ba01b683707a88e9159057
Marcel Derks <marcel.derks@check24.de>
2023-11-23 11:57:38 +0100

    Initial Commit

---
+++ AddressBookSwiftUI/AddressBookSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AddressBookSwiftUIApp.swift
+//  AddressBookSwiftUI
+//
+//  Created by Marcel Derks on 23.11.23.
+//
+
+import SwiftUI
+
+@main
+struct AddressBookSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
